87d22a93c2013e1e12ec187321c246f17a7306b5
titangamerpro <titangamerpro10@gmail.com>
2025-05-11 17:18:14 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="header">
-        <h1>React-Blog</h1>
+        <h1 className='schapka'>React-Blog</h1>
     </header>
   )
 }
